590b843c39615c9286282e4f0b05cf5322ae66b2
Angelo <angelo@ifm.umich.mx>
2019-07-17 13:12:41 -0500

    stemm variable defined inside correct function

--- import_xml.py
+++ import_xml.py
@@ -84,6 +84,7 @@ def stem_tokens(tokens, stemmer):
     return stemmed
 
 def tokenize(text):
+    stemmer = SnowballStemmer('spanish')
     text = ''.join([c for c in text if c not in non_words])
     tokens = word_tokenize(text)
 
@@ -96,63 +97,62 @@ def tokenize(text):
         stem = ['']
     return stem
 
+if __name__ == "__main__": 
+    print('Que operación desea?\n1----->Extraer tweets\n2----->En que idioma están los tweets\n3------>SVM grid serach and fit')
+    opt = input()
+    #extrayendo los tweets del archivo xml
+    if opt=='1':
+        print('--------------------procesando---------------------------------------')
+        tweets_id = process()
 
-print('Que operación desea?\n1----->Extraer tweets\n2----->En que idioma están los tweets\n3------>SVM grid serach and fit')
-opt = input()
-#extrayendo los tweets del archivo xml
-if opt=='1':
-    print('--------------------procesando---------------------------------------')
-    tweets_id = process()
-
-#funciones que detectan el idioma de los tweets
-if opt =='2':
-    print('--------------------detector idiomas---------------------------------------')
-    tweets_id = pd.read_csv('tweets_extraidos_large.csv', encoding='utf-8')
-    tweets_id['first_len_fun'] = tweets_id.tweets.apply(landig_safe)
-    tweets_id['second_len_fun'] = tweets_id.tweets.apply(langdetect)
-    tweets_id['third_len_fun'] = tweets_id.tweets.apply(textblob_safe)
-    #guardando lo anterior
-    tweets_id.to_csv('tweets_extraidos_id_large.csv', encoding='utf-8')
+    #funciones que detectan el idioma de los tweets
+    if opt =='2':
+        print('--------------------detector idiomas---------------------------------------')
+        tweets_id = pd.read_csv('tweets_extraidos_large.csv', encoding='utf-8')
+        tweets_id['first_len_fun'] = tweets_id.tweets.apply(landig_safe)
+        tweets_id['second_len_fun'] = tweets_id.tweets.apply(langdetect)
+        tweets_id['third_len_fun'] = tweets_id.tweets.apply(textblob_safe)
+        #guardando lo anterior
+        tweets_id.to_csv('tweets_extraidos_id_large.csv', encoding='utf-8')
 
 
 
-if opt=='3':
-    print('-------------------------------------------al vectorizdor-----------------------------------------------------')
+    if opt=='3':
+        print('-------------------------------------------al vectorizdor-----------------------------------------------------')
 
-    tweets_id = pd.read_csv('tweets_extraidos_large.csv', encoding='utf-8')
-    #preguntandole al dataframe
-    #tweets_id = tweets_id.query('first_len_fun == "es" or second_len_fun == "es" or third_len_fun == "es"')
-    spanish_stopwords = stopwords.words('spanish')
-    stemmer = SnowballStemmer('spanish')
+        tweets_id = pd.read_csv('tweets_extraidos_large.csv', encoding='utf-8')
+        #preguntandole al dataframe
+        #tweets_id = tweets_id.query('first_len_fun == "es" or second_len_fun == "es" or third_len_fun == "es"')
+        spanish_stopwords = stopwords.words('spanish')
 
-    #iniciaizando 
-    english_stopwords = [line.rstrip('\n') for line in open('english_stopwords.txt')]
-    english_stopwords.extend(['algun', 'com','contr', 'cuand', 'desd', 'dond', 'durant', 'eram', 'estab', 'estais', 'estam', 'estan', 'estand', 'estaran', 'estaras', 'esteis', 'estem', 'esten', 'estes', 'estuv', 'fuer', 'fues', 'fuim', 'fuist', 'hab', 'habr', 'habran', 'habras', 'hast', 'hem', 'hub', 'mas', 'mia', 'mias', 'mio', 'mios', 'much', 'nad', 'nosotr', 'nuestr', 'par', 'per', 'poc', 'porqu', 'qui', 'seais', 'seam', 'sent', 'ser', 'seran', 'seras', 'si', 'sient', 'sint', 'sobr', 'som', 'suy', 'tambien', 'tant', 'ten', 'tendr', 'tendran', 'tendras', 'teng', 'tien', 'tod', 'tuv', 'tuy', 'vosotr', 'vuestr', 'abov', 'becaus', 'befor', 'betw', 'furth', 'hav', 'mor', 'ourselv', 'sam', 'tambi', 'themselv', 'ther', 'thes', 'thos', 'wer', 'wher', 'whil', 'yourselv'])
-    spanish_stopwords.extend(english_stopwords)
-    print(spanish_stopwords)
+        #iniciaizando 
+        english_stopwords = [line.rstrip('\n') for line in open('english_stopwords.txt')]
+        english_stopwords.extend(['algun', 'com','contr', 'cuand', 'desd', 'dond', 'durant', 'eram', 'estab', 'estais', 'estam', 'estan', 'estand', 'estaran', 'estaras', 'esteis', 'estem', 'esten', 'estes', 'estuv', 'fuer', 'fues', 'fuim', 'fuist', 'hab', 'habr', 'habran', 'habras', 'hast', 'hem', 'hub', 'mas', 'mia', 'mias', 'mio', 'mios', 'much', 'nad', 'nosotr', 'nuestr', 'par', 'per', 'poc', 'porqu', 'qui', 'seais', 'seam', 'sent', 'ser', 'seran', 'seras', 'si', 'sient', 'sint', 'sobr', 'som', 'suy', 'tambien', 'tant', 'ten', 'tendr', 'tendran', 'tendras', 'teng', 'tien', 'tod', 'tuv', 'tuy', 'vosotr', 'vuestr', 'abov', 'becaus', 'befor', 'betw', 'furth', 'hav', 'mor', 'ourselv', 'sam', 'tambi', 'themselv', 'ther', 'thes', 'thos', 'wer', 'wher', 'whil', 'yourselv'])
+        spanish_stopwords.extend(english_stopwords)
+        #print(spanish_stopwords)
 
-    vectorizer = CountVectorizer(analyzer = 'word', tokenizer = tokenize, lowercase=True, stop_words=spanish_stopwords)
+        vectorizer = CountVectorizer(analyzer = 'word', tokenizer = tokenize, lowercase=True, stop_words=spanish_stopwords)
 
-    vector = vectorizer.fit_transform(tweets_id.tweets)
+        vector = vectorizer.fit_transform(tweets_id.tweets)
 
-    #ahora se está haciendo el entrenamiento
-    pipeline = Pipeline([('vect', vectorizer), ('cls', LinearSVC())])
+        #ahora se está haciendo el entrenamiento
+        pipeline = Pipeline([('vect', vectorizer), ('cls', LinearSVC())])
 
-    #se va a hacer un barrido de parámetros con la función GridSearchCV
-    parameters = {
-        'vect__max_df':(0.5, 1.9),
-        'vect__min_df':(10, 20, 50),
-        'vect__max_features':(2000),
-        'vect__ngram_range':((1, 1), (1, 2)),
-        'cls__C':(0.2, 0.5, 0.7),
-        'cls__loss':('hinge', 'squared_hinge'),
-        'cls__max_iter':(500, 1000)
-    }
+        #se va a hacer un barrido de parámetros con la función GridSearchCV
+        parameters = {
+            'vect__max_df':(0.5, 2.5),
+            'vect__min_df':(1, 20, 50),
+            'vect__max_features':(500, 1000),
+            'vect__ngram_range':((1, 1), (1, 2)),
+            'cls__C':(0.2, 0.5, 0.7),
+            'cls__loss':('hinge', 'squared_hinge'),
+            'cls__max_iter':(1500, 2000)
+        }
 
-    print('----------------------------y el entrenamiento---------------------')
-    grind_search = GridSearchCV(pipeline, parameters, n_jobs=-1,  verbose=10, scoring='roc_auc')
-    grind_search.fit(tweets_id.tweets, tweets_id.sentiment)
+        print('----------------------------y el entrenamiento---------------------')
+        grind_search = GridSearchCV(pipeline, parameters, n_jobs=-1,  verbose=10, scoring='roc_auc')
+        grind_search.fit(tweets_id.tweets, tweets_id.sentiment)
 
-    print(grind_search.best_params_)
-    best_param = pd.DataFrame(grind_search.best_params_)
-    best_param.to_csv('best_params.csv', encoding='utf-8')
+        print(grind_search.best_params_)
+        best_param = pd.DataFrame(grind_search.best_params_)
+        best_param.to_csv('best_params.csv', encoding='utf-8')
